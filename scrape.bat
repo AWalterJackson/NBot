65ef4d0ea5c459091436b56d14260af9ecbb3d57
Nantang'Itan <nantangitan@gmail.com>
2017-03-07 11:33:41 +1100

    Trying to get patreon remote python call working

---
+++ scrape.bat
@@ -0,0 +1,2 @@
+bash DISPLAY=:0 python scraper.py vempire > vempire.txt
+bash DISPLAY=:0 python scraper.py manyatruenerd > manyatruenerd.txt
--- src/com/nbot/externals/Patreon.java
+++ src/com/nbot/externals/Patreon.java
@@ -3,6 +3,9 @@ package com.nbot.externals;
 import com.nbot.utils.JSONextension;
 import com.nbot.utils.HttpsHandler;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import com.nbot.core.Command;
@@ -11,6 +14,7 @@ import com.nbot.core.CommandBuffer;
 public class Patreon extends Thread {
 
 	private final String CLIENT_NAME = "PATREON";
+	private final String SCRIPT_BASE = "bash DISPLAY=:0 python scraper.py ";
 
 	private CommandBuffer cb;
 	private ArrayList<PatreonCreator> creators;
@@ -22,18 +26,46 @@ public class Patreon extends Thread {
 		this.stayalive = true;
 	}
 
-	public void Run() {
+	public void run() {
 		String pagedata = "";
 		try {
+			generatescraper(creators);
+			getdata();
+			System.out.println("Patreon alert module loaded");
 			while (stayalive) {
-				for (PatreonCreator creator : this.creators) {
-					//TODO Pull data from Python script
-					return;
-				}
+				return;
 			}
+		} catch (IOException e) {
+			System.out.println("Unable to write to scraper.bat");
+			e.printStackTrace();
 		} catch (Exception e) {
 			System.out.println("Exception thrown in Patreon Module.");
+			e.printStackTrace();
 		}
+		return;
+	}
+	
+	private void getdata() throws IOException, InterruptedException{
+		System.out.println(System.getProperty("user.dir")+"\\scrape.bat");
+		Process proc = new ProcessBuilder(System.getProperty("user.dir")+"\\scrape.bat").start();
+		System.out.println("Waiting");
+		proc.waitFor();
+		System.out.println("Terminated");
+	}
+
+	private void generatescraper(ArrayList<PatreonCreator> creators) throws IOException {
+		String script = "";
+		File f = new File("scrape.bat");
+		if (!f.createNewFile()) {
+			f.delete();
+			f.createNewFile();
+		}
+		PrintWriter printer = new PrintWriter(f);
+		for (PatreonCreator creator : creators) {
+			script = script + SCRIPT_BASE + creator.getname() + " > " + creator.getname() + ".txt\n";
+		}
+		printer.write(script);
+		printer.close();
 		return;
 	}
 }
